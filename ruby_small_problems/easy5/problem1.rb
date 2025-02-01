9a118aa8a89683bb0b1f54381d9677c6d31f0f2d
Ref Mags <fyfymags@gmail.com>
2025-02-02 07:34:46 +0900

    one liner for ascii problem

--- ruby_small_problems/easy5/problem1.rb
+++ ruby_small_problems/easy5/problem1.rb
@@ -1,14 +1,10 @@
 def ascii_value(str)
   total = 0
-
-  str.chars do |char|
-    ascii_value = char.ord
-    total += ascii_value
-  end
-  p total
+  str.chars {|char| total += char.ord}
+  total
 end
 
 p ascii_value('Four score') == 984
-# ascii_value('Launch School') == 1251
-# ascii_value('a') == 97
-# ascii_value('') == 0
+p ascii_value('Launch School') == 1251
+p ascii_value('a') == 97
+p ascii_value('') == 0
