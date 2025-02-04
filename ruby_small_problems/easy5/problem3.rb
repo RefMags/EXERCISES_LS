0f11532992aad8dfabb5e28269c99dff340724fe
Ref Mags <fyfymags@gmail.com>
2025-02-05 06:33:11 +0900

    word swap completed

--- ruby_small_problems/easy5/problem3.rb
+++ ruby_small_problems/easy5/problem3.rb
@@ -1,9 +1,26 @@
-def access_word(word)
-  word
+# def swap(str_word)
+#    words = str_word.split
+
+#    swapped_word = words.map do |word|
+#     word[0], word[-1] = word[-1], word[0]
+#     word
+#    end
+
+#    swapped_word.join(" ")
+# end
+
+# p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+# p swap('Abcde') == 'ebcdA'
+# p swap('a') == 'a'
+
+
+def swap(str)
+  str.split.map do |word|
+    first, *mid, last = word.chars
+    "#{last}#{mid.join}#{first}"
+  end.join(' ')
 end
 
-access_word('Oh what a wonderful day it is')
-
-# swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
-# swap('Abcde') == 'ebcdA'
-# swap('a') == 'a'
+p swap('Oh what a wonderful day it is') == 'hO thaw a londerfuw yad ti si'
+p swap('Abcde') == 'ebcdA'
+p swap('a') == 'a'
