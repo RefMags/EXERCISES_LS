20b989b462ad33e876fdfbcc3238b2f91914be7b
Ref Mags <fyfymags@gmail.com>
2025-02-06 06:24:52 +0900

    a different approach to clean-up non-alphabetic characters

--- ruby_small_problems/easy5/problem4.rb
+++ ruby_small_problems/easy5/problem4.rb
@@ -4,10 +4,16 @@ def cleanup(string_word)
   clean_chars = ""
 
   string_word.each_char do |char|
-    if ALPHABET.include?(char)
-      clean_chars << char
-    else
+  #   if ALPHABET.include?(char)
+  #     clean_chars << char
+  #   else
+  #     clean_chars << ' ' unless clean_chars[-1] == ' '
+  #   end
+  # end
+    if !ALPHABET.include?(char)
       clean_chars << ' ' unless clean_chars[-1] == ' '
+    else
+      clean_chars << char
     end
   end
   clean_chars
