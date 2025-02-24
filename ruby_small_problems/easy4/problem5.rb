1faf22dd5b7c6aab0b3bbde7392623fcd75d2751
Ref Mags <fyfymags@gmail.com>
2025-02-24 11:18:30 +0900

    getting multiples of two numbers

--- ruby_small_problems/easy4/problem5.rb
+++ ruby_small_problems/easy4/problem5.rb
@@ -1,17 +1,50 @@
-def multisum(multiple)
+def multisum(limit)
   multiples_3 = []
   multiples_5 = []
   counter = 1
 
-  # multiplying 3, starting with 1, until we get a product of 20
-  loop do
-    multiple_of_3 = 3 * counter
-    break if multiple_of_3 >= multiple
+  while (multiple_of_3 = 3 * counter) <= limit
     multiples_3 << multiple_of_3
-    puts "Counter: #{counter}, Multiple of 3: #{multiple_of_3}, Array: #{multiples_3} ,Multiple: #{multiple}"
     counter += 1
   end
+
+  counter = 1
+  while (multiple_of_5 = 5 * counter) <= limit
+    multiples_5 << multiple_of_5
+    counter += 1
+  end
+
+  p uniq_multiples_of_3_and_5 = (multiples_3 + multiples_5).uniq
+  total = uniq_multiples_of_3_and_5.sum
+  p total
 end
+
+
+## Alternative
+
+# def multisum(limit)
+#   multiples_3 = []
+#   multiples_5 = []
+
+#   # loop do
+#   #     multiple_of_3 = 3 * counter
+#   #     break if multiple_of_3 > limit
+#   #     multiples_3 << multiple_of_3
+#   #     counter += 1
+#   # end
+
+#   # counter = 1
+#   # loop do
+#   #   multiple_of_5 = 5 * counter
+#   #   break if multiple_of_5 > limit
+#   #   multiples_5 << multiple_of_5
+#   #   counter += 1
+#   # end
+
+#   uniq_multiples = (multiples_3 + multiples_5).uniq
+#   total = uniq_multiples.sum
+#   total
+# end
 
 
 multisum(20) #== 3
