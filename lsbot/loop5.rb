8f05ee0a181b93241bc7b7fbadb860be0cf463a3
Ref Mags <fyfymags@gmail.com>
2025-03-01 17:55:35 +0900

    looping questions completed

--- lsbot/loop5.rb
+++ lsbot/loop5.rb
@@ -3,12 +3,12 @@ def pairing_target_sums(arr, target_sum)
 
   arr.each_with_index do |num, idx1|
     (idx1 + 1...arr.size).each do |idx2|
-      result << [num,arr[idx2]] if (num + arr[idx2]) == target_sum
+      result << ([num,arr[idx2]]).sort if (num + arr[idx2]) == target_sum
     end
   end
-  p result
+  p result.sort
 end
 
 
-pairing_target_sums([1, 2, 3, 2], 4)
-# pairing_target_sums([2, 6, 3, 5, 4, 7, 4, 1], 8)
+p pairing_target_sums([1, 2, 3, 2], 4) == [[1, 3], [2, 2]]
+p pairing_target_sums([2, 6, 3, 5, 4, 7, 4, 1], 8) == [[1,7], [2, 6], [3, 5], [4,4]]
