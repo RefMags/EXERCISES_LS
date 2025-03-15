803db11d33cc4ec83bcc350411c8d4445d6560e4
Ref Mags <fyfymags@gmail.com>
2025-03-15 09:59:54 +0900

    refactored title case

--- rb119_sessions/20250315_title_case.rb
+++ rb119_sessions/20250315_title_case.rb
@@ -1,18 +1,26 @@
+# def title_case(str, minor_words=" ")
+#   to_be_title_cased = str.downcase.split
+#   minor_words = minor_words.downcase.split
+
+#   title_cased = to_be_title_cased.map.with_index do |word, idx|
+#     if minor_words.include?(word) && idx == 0
+#       word.capitalize
+#     elsif !minor_words.include?(word)
+#       word.capitalize
+#     elsif minor_words.include?(word)
+#       word
+#     end
+#   end
+
+#   title_cased.join(' ')
+# end
+
 def title_case(str, minor_words=" ")
-  to_be_title_cased = str.downcase.split
-  minor_words = minor_words.downcase.split
+  words = str.downcase.split
+  minors = minor_words.downcase.split
 
-  title_cased = to_be_title_cased.map.with_index do |word, idx|
-    if minor_words.include?(word) && idx == 0
-      word.capitalize
-    elsif !minor_words.include?(word)
-      word.capitalize
-    elsif minor_words.include?(word)
-      word
-    end
-  end
-
-  title_cased.join(' ')
+  words.map.with_index { | word, idx|
+    minors.include?(word) && idx != 0 ? word : word.capitalize }.join(" ")
 end
 
 
