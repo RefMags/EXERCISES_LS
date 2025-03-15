bb7d433de0d5042f08bbd1491354780f32847cbd
Ref Mags <fyfymags@gmail.com>
2025-03-15 09:34:08 +0900

    title case letters leave out the minor words unless they start the string.

---
+++ rb119_sessions/20250315_title_case.rb
@@ -0,0 +1,21 @@
+def title_case(str, minor_words=" ")
+  to_be_title_cased = str.downcase.split
+  minor_words = minor_words.downcase.split
+
+  title_cased = to_be_title_cased.map.with_index do |word, idx|
+    if minor_words.include?(word) && idx == 0
+      word.capitalize
+    elsif !minor_words.include?(word)
+      word.capitalize
+    elsif minor_words.include?(word)
+      word
+    end
+  end
+
+  title_cased.join(' ')
+end
+
+
+p title_case('a clash of KINGS', 'a an the of') == 'A Clash of Kings'
+p title_case('THE WIND IN THE WILLOWS', 'The In') == 'The Wind in the Willows'
+p title_case('the quick brown fox') == 'The Quick Brown Fox'
