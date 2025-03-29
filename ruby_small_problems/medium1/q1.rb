6a06330039bac6b31bd165b57694d5bd706444a4
Ref Mags <fyfymags@gmail.com>
2025-03-29 13:17:59 +0900

    rotating the last element

---
+++ ruby_small_problems/medium1/q1.rb
@@ -0,0 +1,14 @@
+def rotate_array(arr)
+  arr.freeze
+  arr_dup = arr.dup
+  first_ele =  arr_dup.shift
+  arr_dup << first_ele
+end
+
+rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+p rotate_array(x) == [2, 3, 4, 1]   # => true
+p x == [1, 2, 3, 4]                 # => true
