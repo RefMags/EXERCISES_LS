a486a690083e6f55e96232dc111b2230cdcc2c43
Ref Mags <fyfymags@gmail.com>
2025-03-31 16:53:17 +0900

    alternating and zipping

--- ruby_small_problems/easy7/q1.rb
+++ ruby_small_problems/easy7/q1.rb
@@ -1,18 +1,25 @@
+# def interleave(arr1, arr2)
+#   counter = 0
+#   result = []
+
+#   loop do
+#     break if counter == arr1.size
+
+#     result << arr1[counter]
+#     result << arr2[counter]
+#     counter += 1
+#   end
+#   result
+# end
+#
+#
 def interleave(arr1, arr2)
-  counter = 0
   result = []
 
-  loop do
-    break if counter == arr1.size
-
-    result << arr1[counter]
-
-    result << arr2[counter]
-
-    counter += 1
+  arr1.zip(arr2) do |ele|
+    result << ele
   end
-
-  result
+  result.flatten
 end
 
 p interleave([1, 2, 3], ['a', 'b', 'c']) == [1, 'a', 2, 'b', 3, 'c']
