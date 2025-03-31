9fa699c367d8bba6f9737933cf32734c5f0d8f6d
Ref Mags <fyfymags@gmail.com>
2025-03-31 16:54:23 +0900

    navigating position in ruby

--- ruby_small_problems/medium1/q2.rb
+++ ruby_small_problems/medium1/q2.rb
@@ -1,14 +1,24 @@
-def rotate_rightmost_digits(number, target_position)
-  str_number = number.to_s
-  target_num = str_number.slice!(-target_position)
-  str_number << target_num
-  str_number.to_i
+# def rotate_rightmost_digits(number, target_position)
+#   str_number = number.to_s
+#   target_num = str_number.slice!(-target_position)
+#   str_number << target_num
+#   str_number.to_i
+# end
+#
+#
+def rotate_rightmost_digits(number, target)
+  arr_digits = number.to_s.chars.map(&:to_i)
+  target_index = arr_digits.length - target # (0-index)
+
+  # use delete_at to remove and return the element at the target index
+  target_number = arr_digits.delete_at(target_index)
+   result = arr_digits << target_number
+   result.join.to_i
 end
 
-
-# rotate_rightmost_digits(735291, 1) == 735291
-# p rotate_rightmost_digits(735291, 2) == 735219
-rotate_rightmost_digits(735291, 3) == 735912
-# p rotate_rightmost_digits(735291, 4) == 732915
-# p rotate_rightmost_digits(735291, 5) == 752913
-# p rotate_rightmost_digits(735291, 6) == 352917
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
