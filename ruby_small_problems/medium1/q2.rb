e5b8df4551f66cd8bf2cd23aae0828587bc9008e
Ref Mags <fyfymags@gmail.com>
2025-04-03 06:24:28 +0900

    staggered case, alternating between uppercase and lowercase

--- ruby_small_problems/medium1/q2.rb
+++ ruby_small_problems/medium1/q2.rb
@@ -6,19 +6,19 @@
 # end
 #
 #
-def rotate_rightmost_digits(number, target)
-  arr_digits = number.to_s.chars.map(&:to_i)
-  target_index = arr_digits.length - target # (0-index)
+# def rotate_rightmost_digits(number, target)
+#   arr_digits = number.to_s.chars.map(&:to_i)
+#   target_index = arr_digits.length - target # (0-index)
 
-  # use delete_at to remove and return the element at the target index
-  target_number = arr_digits.delete_at(target_index)
-   result = arr_digits << target_number
-   result.join.to_i
-end
+#   # use delete_at to remove and return the element at the target index
+#   target_number = arr_digits.delete_at(target_index)
+#    result = arr_digits << target_number
+#    result.join.to_i
+# end
 
-p rotate_rightmost_digits(735291, 1) == 735291
-p rotate_rightmost_digits(735291, 2) == 735219
-p rotate_rightmost_digits(735291, 3) == 735912
-p rotate_rightmost_digits(735291, 4) == 732915
-p rotate_rightmost_digits(735291, 5) == 752913
-p rotate_rightmost_digits(735291, 6) == 352917
+# p rotate_rightmost_digits(735291, 1) == 735291
+# p rotate_rightmost_digits(735291, 2) == 735219
+# p rotate_rightmost_digits(735291, 3) == 735912
+# p rotate_rightmost_digits(735291, 4) == 732915
+# p rotate_rightmost_digits(735291, 5) == 752913
+# p rotate_rightmost_digits(735291, 6) == 352917
