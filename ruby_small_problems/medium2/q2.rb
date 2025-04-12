1c01a022a9ffb0d4896ee6a0d4cded7226fe88fb
Ref Mags <fyfymags@gmail.com>
2025-04-12 15:07:25 +0900

    find word in a collection of words

---
+++ ruby_small_problems/medium2/q2.rb
@@ -0,0 +1,23 @@
+BLOCKS = ["BO", "XK", "DQ ", "CP", "NA" , "GT", "RE", "FS", "JW", "HU", "VI", "LY","ZM"]
+
+def block_word?(word)
+  result = ""
+  up_word = word.upcase
+
+  BLOCKS.each do |block|
+    if up_word.include?(block[0])
+      result << block[0]
+    elsif up_word.include?(block[1])
+      result << block[1]
+    end
+  end
+
+  up_word.chars.sort == result.chars.sort ? true : false
+
+end
+
+# p block_word?('BATCH') == true
+# p block_word?('BUTCH') == false
+# p block_word?('jest') == true
+block_word?('apples') #== false
+# p block_word?('Baby') == false
