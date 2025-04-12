41900d305652b7ae6c9657ad53a1308f0501d1ac
Ref Mags <fyfymags@gmail.com>
2025-04-12 16:47:24 +0900

    letter percentages determined

--- ruby_small_problems/medium2/q3.rb
+++ ruby_small_problems/medium2/q3.rb
@@ -19,11 +19,9 @@ def letter_percentages(str)
     (value / size) * 100
   end
 
-  # p update_value
   update_value.each_with_index do |value, idx|
     if idx == 0
       HASH[:lowercase] = value
-      # puts "#{HASH[:lowercase]} updated hash is #{HASH}"
     elsif idx == 1
       HASH[:uppercase] = value
     else
@@ -34,6 +32,6 @@ def letter_percentages(str)
 end
 
 
-p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
+# p letter_percentages('abCdef 123') == { lowercase: 50.0, uppercase: 10.0, neither: 40.0 }
 p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25.0 }
-p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
+# p letter_percentages('123') == { lowercase: 0.0, uppercase: 0.0, neither: 100.0 }
