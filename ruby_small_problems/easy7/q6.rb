a417b2384cb11108a42cc8bb40ab685537aff4b7
Ref Mags <fyfymags@gmail.com>
2025-04-20 07:21:45 +0900

    adding problems from ruby small problems

--- ruby_small_problems/easy7/q6.rb
+++ ruby_small_problems/easy7/q6.rb
@@ -1,17 +1,26 @@
-p ALPHABET = ("a".."z").to_a + ("A".."Z").to_a
+ALPHABET = ("a".."z").to_a + ("A".."Z").to_a
 
 def staggered_case(str)
-  result = ""
+  result = []
 
-  str.each_char do |char|
+  str.chars do |char|
     if ALPHABET.include?(char)
-      if result[-1] == result[-1].upcase
-
+      last_ele = result.reverse.find do |ele|
+        p ALPHABET.include?(ele)
       end
+      last_ele
+        # if last_ele == last_ele.upcase
+        #   result << char.downcase
+        # else
+        #   result << char.upcase
+        # end
+    else
+      result << char
     end
   end
+  result
 end
 
-staggered_case('I Love Launch School!') == 'I lOvE lAuNcH sChOoL!'
-staggered_case('ALL CAPS') == 'AlL cApS'
-staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
+staggered_case('I Love Launch School!') #== 'I lOvE lAuNcH sChOoL!'
+# staggered_case('ALL CAPS') == 'AlL cApS'
+# staggered_case('ignore 77 the 444 numbers') == 'IgNoRe 77 ThE 444 nUmBeRs'
---
+++ ruby_small_problems/easy7/q7.rb
@@ -0,0 +1,9 @@
+def show_multiplicative_average(numbers)
+  product = numbers.inject(:*) / numbers.size
+  average = pro
+end
+
+
+show_multiplicative_average([3, 5])                # => The result is 7.500
+show_multiplicative_average([6])                   # => The result is 6.000
+show_multiplicative_average([2, 5, 7, 11, 13, 17]) # => The result is 28361.667
